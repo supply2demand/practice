6d1beba3b8f75ead4b8932a924edfe0e0c0d520d
supply2demand <129346440+supply2demand@users.noreply.github.com>
2023-12-10 21:28:24 +0900

    크롬 검색 자동화.py
    코멘트 추가

--- test2.py
+++ test2.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
+options.add_experimental_option("detach", True) #Chrome이 자동으로 꺼지는 것을 막음
 browser = webdriver.Chrome(options= options)
-browser.get("https://naver.com")
-browser.find_element(By.ID,"query").send_keys("도재승")
-browser.find_element(By.ID,"search-btn").click()
+browser.get("https://naver.com") #해당 사이트를 시작으로 브라우저 실행
+browser.find_element(By.ID,"query").send_keys("검색어") # 검색창 선택후, 검색 key 입력
+browser.find_element(By.ID,"search-btn").click()  #검색 버튼 클릭
